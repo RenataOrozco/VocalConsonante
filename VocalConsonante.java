a2ccad874a8d5bf38338242d16a4607a60939f3f
TristanLira <24030458@itcelaya.edu,mx>
2024-11-13 20:04:43 -0600

    se modifico el codigo para leer solo un caracter y determinar que es, en vez de una frase completa. Además se agrego un metodo que determina si el input es una sola letra para evitar errores.

--- VocalConsonante.java
+++ VocalConsonante.java
@@ -5,8 +5,9 @@ public class VocalConsonante {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Introduce una frase: ");
-        String ph2 = new java.util.Scanner(System.in).nextLine();
+        System.out.print("Introduzca su caracter: ");
+        
+        String ph2 = sc.nextLine();
         Solution ex2 = new Solution(ph2);
         
         String result2 = ex2.clasification();
@@ -21,28 +22,44 @@ class Solution {
     public Solution(String ph) {
         this.ph = ph;
     }
-
+    
+    
+    public boolean isOneCharacter() {
+        if(ph.length() == 1 && Character.isLetter(ph.charAt(0)) == true) {
+            return true;
+        }
+        return false;
+    }
+    
+    
     public String clasification() {
-        int vowels = 0;
-        int consonants = 0;
-        int symbols = 0;
-
-        ph = ph.toLowerCase();
-
-        for (char ch : ph.toCharArray()) {
-            if (Character.isLetter(ch)) {
-                if ("aeiou".indexOf(ch) != -1) {
-                    vowels++;
-                } else {
-                    consonants++;
-                }
-            } else if (!Character.isWhitespace(ch)) {
-                symbols++;
+        char[] vowels = {'a', 'e', 'i', 'o', 'u', 'á', 'é', 'í', 'ó', 'ú'};
+        char[] consonants = {'b', 'c', 'd', 'f', 'g', 'h', 'j','k',
+                             'l', 'm', 'n', 'ñ', 'p', 'q', 'r',
+                             's', 't', 'v', 'w', 'x', 'y', 'z'};
+        
+        char input;
+        
+        if(isOneCharacter() == false) {
+            return "No es una letra.";
+        } 
+        /*evalua si la entrada es de una sola letra,
+        para no ejecutar el resto del codigo*/
+        
+        input = ph.charAt(0);
+        
+        for(char i: vowels) {
+            if(Character.toLowerCase(input) == i) {
+                return "Es vocal.";
             }
         }
-
-        return "Número de vocales: " + vowels + "\n"
-                + "Número de consonantes: " + consonants + "\n"
-                + "Número de símbolos: " + symbols;
+        
+        for(char i: consonants) {
+            if(Character.toLowerCase(input) == i) {
+                return "Es consonante.";
+            }
+        }
+        
+        return "No es una letra.";
     }
 }
